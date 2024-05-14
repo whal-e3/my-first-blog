0c82282485bd7846a926644d50ec2ac63a647f3d
ericwhale <ericwhale0@gmail.com>
2024-05-14 17:19:23 +0900

    dsafdsa

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
+    path(r'', include('blog.urls')),
 ]
