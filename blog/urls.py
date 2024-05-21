6b1683ec1625ba496404bb001dd11beee843cf1e
ericwhale <ericwhale0@gmail.com>
2024-05-21 10:36:41 +0000

    edit...

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
 ]
